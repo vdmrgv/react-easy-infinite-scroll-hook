6cc012ef4055a8890fafc95635a84e9f2783e6e0
Vadim Rogov <rogov.2099@yandex.com>
2022-07-13 02:33:04 +0300

    fix - `cjs` export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import useInfiniteScroll from './useInfiniteScroll';
 
 export { default as InfiniteScroll } from './InfiniteScroll';
+export { default as useInfiniteScroll } from './useInfiniteScroll';
 export * from './types';
 
 export default useInfiniteScroll;
