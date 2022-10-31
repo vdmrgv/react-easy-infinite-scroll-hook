bd2e75022fad4f200ac31fe4a41386e8b982306a
Vadim Rogov <54108395+vdmrgv@users.noreply.github.com>
2022-10-31 21:06:47 +0300

    build: fix rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,7 +5,7 @@ import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import external from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
-import pkg from './package.json';
+import pkg from './package.json' assert { type: 'json' };
 
 export default [
   {
